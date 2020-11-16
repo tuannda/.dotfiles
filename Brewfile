d5eb9fd8b702eaa643e244ccd286e73a0430e2f8
Petr Nguyen <tuannda38@gmail.com>
2020-11-16 21:20:36 +0700

    .

---
+++ Brewfile
@@ -0,0 +1,53 @@
+# Taps
+tap 'homebrew/cask'
+tap 'homebrew/cask-fonts'
+tap 'homebrew/cask-versions'
+tap 'homebrew/bundle'
+
+# Binaries
+brew 'awscli'
+brew 'ffmpeg'
+brew 'gh'
+brew 'git'
+brew 'gnupg2'
+brew 'grep'
+brew 'httpie'
+
+# Medialibrary
+brew 'jpegoptim'
+brew 'optipng'
+brew 'pngquant'
+brew 'svgo'
+brew 'gifsicle'
+
+# Development
+brew 'php'
+brew 'composer'
+brew 'imagemagick'
+brew 'mysql'
+brew 'nginx'
+brew 'node'
+brew 'redis'
+brew 'yarn'
+
+# Apps
+cask 'docker'
+cask 'firefox'
+cask 'google-chrome'
+cask 'imageoptim'
+cask 'phpstorm'
+cask 'screenflow'
+cask 'skype'
+cask 'telegram-desktop'
+cask 'tinkerwell'
+cask 'visual-studio-code'
+cask 'vlc'
+
+# Fonts
+cask 'font-lato'
+cask 'font-open-sans'
+cask 'font-roboto'
+cask 'font-source-code-pro-for-powerline'
+cask 'font-source-code-pro'
+cask 'font-source-sans-pro'
+cask 'font-source-serif-pro'
---
+++ my.sh
@@ -0,0 +1,43 @@
+# Load Composer tools
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+
+# Load Node global installed binaries
+export PATH="$HOME/.node/bin:$PATH"
+
+# Use project specific binaries before global ones
+export PATH="node_modules/.bin:vendor/bin:$PATH"
+
+# Shortcuts
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+
+# PHP
+alias composer="php -d memory_limit=-1 /usr/local/bin/composer"
+
+# Vagrant
+alias v="vagrant global-status"
+alias vup="vagrant up"
+alias vhalt="vagrant halt"
+alias vssh="vagrant ssh"
+alias vreload="vagrant reload"
+alias vrebuild="vagrant destroy --force && vagrant up"
+
+# Docker
+alias docker-composer="docker-compose"
+
+# Git
+alias gst="git status"
+alias gb="git branch"
+alias gc="git checkout"
+alias gl="git log --oneline --decorate --color"
+alias amend="git add . && git commit --amend --no-edit"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias force="git push --force"
+alias nuke="git clean -df && git reset --hard"
+alias pop="git stash pop"
+alias pull="git pull"
+alias push="git push"
+alias resolve="git add . && git commit --no-edit"
+alias stash="git stash -u"
+alias unstage="git restore --staged ."
+alias wip="commit wip"
